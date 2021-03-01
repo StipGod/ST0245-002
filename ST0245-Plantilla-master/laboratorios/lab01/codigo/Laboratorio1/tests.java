ac1b12a9ed24b5c3ed857bd80e5067f5f1c1c9b0
StipGod <72163911+StipGod@users.noreply.github.com>
2021-03-01 17:57:52 -0500

    Add files via upload

--- ST0245-Plantilla-master/laboratorios/lab01/codigo/Laboratorio1/tests.java
+++ ST0245-Plantilla-master/laboratorios/lab01/codigo/Laboratorio1/tests.java
@@ -21,9 +21,9 @@ public class tests {
         }
     }
     public static String cadenaAleatoria(int longitud) {
-        // El banco de caracteres
+        
         String banco = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
-        // La cadena en donde iremos agregando un carácter aleatorio
+        
         String cadena = "";
         for (int x = 0; x < longitud; x++) {
             int indiceAleatorio = numeroAleatorioEnRango(0, banco.length() - 1);
@@ -33,7 +33,7 @@ public class tests {
         return cadena;
     }
     public static int numeroAleatorioEnRango(int minimo, int maximo) {
-        // nextInt regresa en rango pero con límite superior exclusivo, por eso sumamos 1
+
         return ThreadLocalRandom.current().nextInt(minimo, maximo + 1);
     }
 }
